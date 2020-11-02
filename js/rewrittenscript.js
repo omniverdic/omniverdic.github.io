085e8dd4d55c80c9e0fe124cea802d25a2c5b985
ItzFlarezz <quangsang2002@gmail.com>
2020-11-02 23:07:34 +0100

    Måndag 02/11

--- js/rewrittenscript.js
+++ js/rewrittenscript.js
@@ -11,109 +11,123 @@ var partcounter = 0;
 
 
 //Användarinformation (namn, ålder och biologiskt kön)
-var name;
+var reactant;  //ändrade namnvariabel till reactant istöllet för name då name tydligen är ett reserverat ord i js.
 var age;
-var biologicalsex;
+var biologicalSex;
 
 
 //Nersparning av reaktionstider på röd (#ee4117)
-var redreaction1 = 0;
-var redreaction2 = 0;
-var redreaction3 = 0;
-var redreaction4 = 0;
-var redreaction5 = 0;
+var redReaction1 = 0;
+var redReaction2 = 0;
+var redReaction3 = 0;
+var redReaction4 = 0;
+var redReaction5 = 0;
 
 //Nersparning av reaktionstider på gul (#fcf60a)
-var yellowreaction1 = 0;
-var yellowreaction2 = 0;
-var yellowreaction3 = 0;
-var yellowreaction4 = 0;
-var yellowreaction5 = 0;
+var yellowReaction1 = 0;
+var yellowReaction2 = 0;
+var yellowReaction3 = 0;
+var yellowReaction4 = 0;
+var yellowReaction5 = 0;
 
 //Nersparning av reaktionstider på blå (#0267ad)
-var bluereaction1 = 0;
-var bluereaction2 = 0;
-var bluereaction3 = 0;
-var bluereaction4 = 0;
-var bluereaction5 = 0;
+var blueReaction1 = 0;
+var blueReaction2 = 0;
+var blueReaction3 = 0;
+var blueReaction4 = 0;
+var blueReaction5 = 0;
 
 //Nersparning av reaktionstider på orange (#e17000)
-var orangereaction1 = 0;
-var orangereaction2 = 0;
-var orangereaction3 = 0;
-var orangereaction4 = 0;
-var orangereaction5 = 0;
+var orangeReaction1 = 0;
+var orangeReaction2 = 0;
+var orangeReaction3 = 0;
+var orangeReaction4 = 0;
+var orangeReaction5 = 0;
 
 //Nersparning av reaktionstider på rosa (#ff69b4)
-var pinkreaction1 = 0;
-var pinkreaction2 = 0;
-var pinkreaction3 = 0;
-var pinkreaction4 = 0;
-var pinkreaction5 = 0;
+var pinkReaction1 = 0;
+var pinkReaction2 = 0;
+var pinkReaction3 = 0;
+var pinkReaction4 = 0;
+var pinkReaction5 = 0;
 
 //Nersparning av reaktionstider på 262Hz (Middle C, C4)
-var hz262reaction1 = 0;
-var hz262reaction2 = 0;
-var hz262reaction3 = 0;
-var hz262reaction4 = 0;
-var hz262reaction5 = 0;
+var hz262Reaction1 = 0;
+var hz262Reaction2 = 0;
+var hz262Reaction3 = 0;
+var hz262Reaction4 = 0;
+var hz262Reaction5 = 0;
 
 //Nersparning av reaktionstider på 1047Hz (C6)
-var hz1047reaction1 = 0;
-var hz1047reaction2 = 0;
-var hz1047reaction3 = 0;
-var hz1047reaction4 = 0;
-var hz1047reaction5 = 0;
+var hz1047Reaction1 = 0;
+var hz1047Reaction2 = 0;
+var hz1047Reaction3 = 0;
+var hz1047Reaction4 = 0;
+var hz1047Reaction5 = 0;
 
 //Nersparning av reaktionstider på 4186Hz (Highest C, C8)
-var hz4186reaction1 = 0;
-var hz4186reaction2 = 0;
-var hz4186reaction3 = 0;
-var hz4186reaction4 = 0;
-var hz4186reaction5 = 0;
+var hz4186Reaction1 = 0;
+var hz4186Reaction2 = 0;
+var hz4186Reaction3 = 0;
+var hz4186Reaction4 = 0;
+var hz4186Reaction5 = 0;
 
 
 //Variabler till nödvändiga element
-var firstpage = document.getElementsByClassName("firstpage")[0];
-var toinfocollection = firstpage.getElementsByTagName("button")[0];
+var firstPage = document.getElementsByClassName("firstpage")[0];
+var toInfoCollection = firstPage.getElementsByTagName("button")[0];
+var nameInput = document.getElementById("nameinput");
+var ageInput = document.getElementById("agecollection")
+var genderInput = document.getElementById("gendercollection")
 
-var infocollection = document.getElementsByClassName("infocollection")[0];
-var infosubmit = infocollection.getElementsByTagName("button")[0];
+var infoCollection = document.getElementsByClassName("infocollection")[0];
+var infoSubmit = infoCollection.getElementsByTagName("button")[0];
 
 var entire = document.getElementsByClassName("entire")[0];
-var entirebtn = entire.getElementsByTagName("button")[0];
+var entireBtn = entire.getElementsByTagName("button")[0];
 
 var wait = document.getElementsByClassName("wait")[0];
 
 var tap = document.getElementsByClassName("tap")[0];
 
-var toofast = document.getElementsByClassName("toofast")[0];
-var toofastbtn = toofast.getElementsByTagName("button")[0]; 
+var tooFast = document.getElementsByClassName("toofast")[0];
+var tooFastBtn = tooFast.getElementsByTagName("button")[0]; 
 
 var result = document.getElementsByClassName("result")[0];
-var resultbtn = result.getElementsByTagName("button")[0];
-var resultf = result.getElementsByTagName("res")[0];
+var resultBtn = result.getElementsByTagName("button")[0];
+var resultF = result.getElementsByTagName("res")[0];
 
 //onclick handler functions
 function firstPageToInfo() {
-    firstpage.setAttribute("dn","");
-    infocollection.removeAttribute("dn");
+  firstPage.setAttribute("dn","");
+  infoCollection.removeAttribute("dn");
 }
 
 function infoToFirstPage() {
-    infocollection.setAttribute("dn", "");
-    firstpage.removeAttribute("dn");
-}
-
-function infotoEntire() {
-    infocollection.setAttribute("dn", "");
-    entire.removeAttribute("dn")
+  infoCollection.setAttribute("dn", "");
+  firstPage.removeAttribute("dn");
 }
 
 function submitInfo() {
-  if(document.getElementById("namecollection") == "")
+  if(document.forms["namecollection"].nameinput.value === ""){
+    alert("Please fill out the information requested.");
+    return false;
+  } 
+  else if(document.forms["agecollection"].ageinput.value === "Default") {
+    alert("Please fill out the information requested2");
+    return false;
+  }
+  else if(document.forms["gendercollection"].genderinput.value === "Default") {
+    alert("Please fill out the information requested3");
+    return false;
+  }
+    reactant = nameinput;
+    age = ageInput;
+    biologicalSex = genderInput;
+    infoCollection.setAttribute("dn", "");
+    entire.removeAttribute("dn");
+    return true;
 }
-
 
 //mobile detection
 if (
